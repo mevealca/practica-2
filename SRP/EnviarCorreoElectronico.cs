2b8f8de5d1b36c8def7ca7de5f46d1331906affc
Miguel Eve <miguel.eve@mem.gob.do>
2024-04-28 12:15:17 -0400

    Agrega la solución para implementar SRP a la clase de persona

---
+++ SRP/EnviarCorreoElectronico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class EnviarCorreoElectronico<Tdata> : IEnviarCorreos<Tdata>
+    {
+        public void EnviarCorreo(Tdata data)
+        {
+            Console.WriteLine("especificamos que correo deseamos enviar");
+        }
+    }
+}
---
+++ SRP/Persona.cs
@@ -0,0 +1,26 @@
+﻿namespace SRP
+{
+    public abstract class Persona
+    {
+        private string nombre;
+        private string edad;
+        private string direccion;
+        private string correoElectronico;
+
+        public Persona (string nombre, string edad, string direccion, string correoElectronico)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.direccion = direccion;
+            this.correoElectronico = correoElectronico;
+        }
+
+        public virtual void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Direccion:{direccion}");
+            Console.WriteLine($"Correo electronico: {correoElectronico}");
+        }
+    }
+}
